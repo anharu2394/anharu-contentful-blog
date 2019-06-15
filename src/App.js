ea937d62e37cced7a36fef01c5f159af0c8d1ca4
Anharu <haruan2394@gmail.com>
2019-06-15 16:57:48 +0900

    Change: Update App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { Subscribe, Provider } from 'unstated';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import AllPostsContainer from './containers/AllPostsContainer';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Route exact path='/' render={() => <h1>a</h1>} />
-        <Route path='/about' render={() => <h1>OK</h1>} />
-      </div>
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <div>
+          <Route exact path='/' render={() => (
+            <Subscribe to={[AllPostsContainer]}>
+              {
+                props => <Home {...props}/>
+              }
+            </Subscribe>
+          )} />
+          <Route path='/about' render={() => <h1>OK</h1>} />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
