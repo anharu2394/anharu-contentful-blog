f61e0169f9b7801b3f3590df91dada6cc1438785
Anharu <haruan2394@gmail.com>
2019-06-16 18:04:12 +0900

    Change: Update App component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import { Subscribe, Provider } from 'unstated';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SmallHeader from './components/SmallHeader';
 import Home from './components/Home';
+import PostPage from './components/PostPage';
 import AllPostsContainer from './containers/AllPostsContainer';
+import PostContainer from './containers/PostContainer';
 
 function App() {
   return (
     <Provider>
       <BrowserRouter>
         <div>
+          <Route exact path='/' render={() => <Header />} />
+          <Route exact path='/posts/:id' render={() => <SmallHeader />} />
           <Route exact path='/' render={() => (
             <Subscribe to={[AllPostsContainer]}>
               {
@@ -18,7 +25,15 @@ function App() {
               }
             </Subscribe>
           )} />
-          <Route path='/about' render={() => <h1>OK</h1>} />
+          <Route exact path='/about' render={() => <h1>OK</h1>} />
+          <Route exact path="/posts/:id" render={router => (
+            <Subscribe to={[PostContainer]}>
+              {
+                props => <PostPage {...props} id={router.match.params.id} />
+              }
+            </Subscribe>
+          )} />
+          <Footer />
         </div>
       </BrowserRouter>
     </Provider>
