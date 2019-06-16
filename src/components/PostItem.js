c92765a500d6a16ec47efa5ced5e54537b64a107
Anharu <haruan2394@gmail.com>
2019-06-16 18:03:49 +0900

    Change: Update PostItem

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const PostItem = ({post: post}) => (
-  <div>{post.title}</div>
-);
+const PostItem = ({post: post}) => {
+  const date = new Date(post.fields.date);
+  return (
+    <Wrapper>
+      <StyledLink to="/posts/first-post">
+        <Image src={post.fields.image.fields.file.url}/>
+        <Detail>
+          <DateText>{date.getFullYear()}.{date.getMonth() + 1}.{date.getDate()}</DateText>
+          <Title>{post.fields.title}</Title>
+        </Detail>
+      </StyledLink>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 25%;
+  margin: 40px 20px;
+  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.12), 0 2px 3px 0 rgba(0, 0, 0, 0.22);
+`;
+
+const Image = styled.img`
+  display: block; 
+  width:100%;
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const DateText = styled.p`
+  margin: 2px;
+  font-size: .7rem;
+  color: #b5b5b5;
+  font-weight: 900;
+`;
+
+const Detail = styled.div`
+  background: #fff;
+  height: 100px;
+  padding: 30px;
+`;
+
+const Title = styled.h2`
+  margin: 0;
+  color: #222;
+  font-size: 1.3rem;
+`;
 
 export default PostItem;
