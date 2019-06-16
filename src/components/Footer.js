0c411d6b05a105e378c8032c1b66698054bb5aad
Anharu <haruan2394@gmail.com>
2019-06-16 18:01:41 +0900

    Change: Update Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import PostItem from './PostItem';
+
+const FooterComponent = () => (
+  <Footer>
+      <Inner>
+        <Message>© Copyright 2019 Ando Haruka All rights reserved.</Message>
+      </Inner>
+  </Footer>
+);
+
+const Footer = styled.footer`
+  height: 140px;
+  padding-top: 10px;
+  background-color: #f7f7f7;
+`;
+
+const Inner = styled.div`
+  height: calc(100% - 15px);
+  background: linear-gradient(#f7f7f7 0%,rgba(240,150,210,0.4) 100%);
+`;
+
+const Message = styled.p`
+  color: rgba(240,150,210);
+  padding-top: 40px;
+  text-align: center;
+`;
+
+export default FooterComponent;
