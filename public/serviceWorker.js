ba99d49c84c7b823fa6f282304d78cfa50a74853
Anharu <haruan2394@gmail.com>
2019-07-25 09:46:31 -0700

    Change: Update serviceWorker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,13 +1,16 @@
-if (evt.request.mode !== 'navigate') {
-  // Not a page navigation, bail.
-  return;
-}
-evt.respondWith(
-    fetch(evt.request)
-        .catch(() => {
-          return caches.open(CACHE_NAME)
-              .then((cache) => {
-                return cache.match('index.html');
-              });
-        })
-);
+self.addEventListener('fetch', (evt) => {
+  console.log('[ServiceWorker] Fetch', evt.request.url);
+  if (evt.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+    return;
+  }
+  evt.respondWith(
+      fetch(evt.request)
+          .catch(() => {
+            return caches.open(CACHE_NAME)
+                .then((cache) => {
+                  return cache.match('index.html');
+                });
+          })
+  );
+});
